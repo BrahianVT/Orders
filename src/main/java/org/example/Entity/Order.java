e5d7dcc8e4b474a5bd1b98c461797dc552eb7b90
Brahian Velázquez Téllez <pumasemj@hotmail.com>
2021-04-23 16:28:41 -0500

    added entity orders

---
+++ src/main/java/org/example/Entity/Order.java
@@ -0,0 +1,56 @@
+package org.example.Entity;
+
+public class Order {
+    private String id;
+    private String name;
+    private String temp;
+    private int shelfLife;
+    private float decayRate;
+    public  float orderAge;
+    private Thread lifeRemainig;
+    private boolean run = true;
+    public Order(){}
+    public Order(String id, String name, String temp, int shelfLife, float decayRate) {
+        this.id = id;
+        this.name = name;
+        this.temp = temp;
+        this.shelfLife = shelfLife;
+        this.decayRate = decayRate;
+    }
+
+    public float calculateRemainingTime(int shelfDecayModifier){
+        return  shelfLife - orderAge - decayRate * orderAge * shelfDecayModifier /shelfLife;
+    }
+    public void initTime(){
+         Runnable taskOrderAge  = () -> {
+             try {
+                 while(run){ orderAge++;  Thread.sleep(1000); }
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         };
+        lifeRemainig = new Thread(taskOrderAge);
+        lifeRemainig.start();
+    }
+
+    public void finishTime(){ this.run = false; }
+    public String getId() { return id;  }
+
+    public void setId(String id) {  this.id = id;  }
+
+    public String getName() {  return name;  }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getTemp() { return temp; }
+
+    public void setTemp(String temp) { this.temp = temp; }
+
+    public int getShelfLife() { return shelfLife; }
+
+    public void setShelfLife(int shelfLife) { this.shelfLife = shelfLife; }
+
+    public double getDecayRate() { return decayRate; }
+
+    public void setDecayRate(float decayRate) { this.decayRate = decayRate; }
+}
